b1beaf6fc61bd6ed74afd89fc33e1516ed5e9317
Kenny Baron <kennybaron@fastmail.com>
2025-01-19 22:06:57 -0500

    try copy pdf build script

---
+++ copy-pdfs.mjs
@@ -0,0 +1,24 @@
+import { copyFile, readdir } from 'fs/promises';
+import { join } from 'path';
+
+async function copyPDFs() {
+  const assetsDir = 'src/assets/cms';
+  const targetDir = 'dist/';
+
+  try {
+    await mkdir(targetDir, { recursive: true });
+
+    const files = await readdir(assetsDir);
+    for (const file of files) {
+      if (file.endsWith('.pdf')) {
+        await copyFile(join(assetsDir, file), join(targetDir, file));
+      }
+      console.log(`Copied ${file} to dist/`);
+    }
+  } catch (err) {
+    console.error('Error copying PDFs:', err);
+    process.exit(1);
+  }
+}
+
+await copyPDFs();
--- src/constants/info.ts
+++ src/constants/info.ts
@@ -1,2 +1,2 @@
-export const EMAIL = "frontierperformance8@gmail.com"
-export const PHONE = "7863851746"
+export const EMAIL = 'frontierperformance8@gmail.com';
+export const PHONE = '7863851746';
